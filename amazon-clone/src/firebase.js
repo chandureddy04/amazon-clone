0fe392f915b71cba9b98876db8848358bf1f3002
Chandu Singireddy <chandusingireddy04@gmail.com>
2021-08-25 00:20:33 +0530

    commit on main

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEbr0IpIan8eRPyNXRM4A0VTuiB_UpeDM",
+    authDomain: "challenge-907aa.firebaseapp.com",
+    projectId: "challenge-907aa",
+    storageBucket: "challenge-907aa.appspot.com",
+    messagingSenderId: "265064891846",
+    appId: "1:265064891846:web:58fdacdcf41a3279f9dec7"
+};
